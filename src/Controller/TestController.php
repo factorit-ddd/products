7e2d81a2d62aad77c721d8abf7e1f1dee323c078
Alex Brouwer <alex@factorit.nl>
2022-01-04 08:09:53 +0000

    open 0.3.x-dev

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,13 +3,19 @@ declare(strict_types=1);
 
 namespace Products\Controller;
 
-use Jifeline\Products\Core\Application\Command\CreateProduct;
+use Jifeline\Products\Domain\Product;
+use Jifeline\Products\Domain\ProductId;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
+    #[Route(path: "/test", methods: ["GET"])]
     public function testAction(): Response
     {
-        $command = new CreateProduct();
+        $data = Product::create(ProductId::generate());
+
+        return new JsonResponse($data->toArray());
     }
 }
